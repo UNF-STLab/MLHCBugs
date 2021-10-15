acce95323bca67df934c80ac3be53a56a090e583
David Wisnosky <DJWisnosky@icloud.com>
2021-10-15 13:22:30 -0400

    IDs now in JSON dict, bug runs added.

--- MLHCBugs_Set_ID.py
+++ MLHCBugs_Set_ID.py
@@ -3,302 +3,46 @@
 
 import os
 import argparse
+import json
 
 def get_commit_id(repo_name:str):
 
     commit_id_file = open('Repos/' + repo_name + '/.git/head', 'r')
     commit_id = commit_id_file.read()
 
-def bug_test_breast_cancer_classifier(bug_id:int):
+def set_id_run_bug(bug_id:str , repo_name:str, commit_id:str):
 
-    commit_id = get_commit_id('breast_cancer_classifier')
+    current_commit_id = get_commit_id(repo_name)
 
-    if bug_id == 1:
+    if current_commit_id != commit_id:
+        os.system('cd Repos/' + repo_name + '; git checkout ' + commit_id)
 
-        if commit_id != 'dc30252de503a87d1c4ad9ba32eacef6efca1d8d':
-           os.system('cd Repos/breast_cancer_classifier; git checkout ' +
-                        'dc30252de503a87d1c4ad9ba32eacef6efca1d8d')
-
-        os.system('./BugRuns/bug_id_1.sh')
-
-def bug_test_medical_zoo_pytorch(bug_id:int):
-
-    commit_id = get_commit_id('MedicalZooPytorch')
-
-    if bug_id == 2:
-
-        if commit_id != '467f0c02f0468de127b814a52d3b05151e8380c6':
-           os.system('cd Repos/MedicalZooPytorch; git checkout ' +
-                        '467f0c02f0468de127b814a52d3b05151e8380c6')
+    os.system('./BugRuns/bug_id_' + bug_id + '.sh')
         
-        os.system('./BugRuns/bug_id_2.sh')
-
-    if bug_id == 3:
-
-        if commit_id != '4a90346c3d6e445712089c6bf13cbae00b43c35b':
-           os.system('cd Repos/MedicalZooPytorch; git checkout ' + 
-                        '4a90346c3d6e445712089c6bf13cbae00b43c35b')
-
-        os.system('./BugRuns/bug_id_3.sh')
-
-def bug_test_RGAN(bug_id: int):
-
-    commit_id = get_commit_id('RGAN')
-
-    if bug_id == 4:
-
-        if commit_id != '6e1ee6b484f0d26b13b952d154482cd7931eb635':
-           os.system('cd Repos/RGAN; git checkout ' +
-                     '6e1ee6b484f0d26b13b952d154482cd7931eb635')
-
-        os.system('./BugRuns/bug_id_4.sh')
-    
-def bug_test_nilearn(bug_id: int):
-
-    commit_id = get_commit_id('nilearn')
-
-    if bug_id == 5:
-
-        if commit_id != '2425238d4479fe1b6bee34227a4d95c252cb73e5':
-           os.system('cd Repos/nilearn; git checkout ' +
-                     '2425238d4479fe1b6bee34227a4d95c252cb73e5')
-
-        os.system('./BugRuns/bug_id_5.sh')
-
-    if bug_id == 6:
-
-        if commit_id != '89b4e799a229ca4b1a11a12d9e486fa0cc3ab274':
-           os.system('cd Repos/nilearn; git checkout ' +
-                     '89b4e799a229ca4b1a11a12d9e486fa0cc3ab274')
-
-        os.system('./BugRuns/bug_id_6.sh')
-
-    if bug_id == 7:
-
-        if commit_id != 'ac1a934e3b2b4061f894b518c960412df9ea4f11':
-           os.system('cd Repos/nilearn; git checkout ' +
-                     'ac1a934e3b2b4061f894b518c960412df9ea4f11')
-
-        os.system('./BugRuns/bug_id_7.sh')
-
-    if bug_id == 8:
-
-        if commit_id != 'f47d6f58577d2b3bec51c3eba2e35dac3ad625c6':
-           os.system('cd Repos/nilearn; git checkout ' +
-                     'f47d6f58577d2b3bec51c3eba2e35dac3ad625c6')
-
-        os.system('./BugRuns/bug_id_8.sh')
-
-    if bug_id == 9:
-
-        if commit_id != '01647c2c0d69a227124d8834a0bbb46a6acf0927':
-           os.system('cd Repos/nilearn; git checkout ' +
-                     '01647c2c0d69a227124d8834a0bbb46a6acf0927')
-
-        os.system('./BugRuns/bug_id_9.sh')
-
-    if bug_id == 10:
-
-        if commit_id != 'd32890c2477d82848aee953156b27e14559b21eb':
-           os.system('cd Repos/nilearn; git checkout ' +
-                     'd32890c2477d82848aee953156b27e14559b21eb')
-
-        os.system('./BugRuns/bug_id_10.sh')
-
-    if bug_id == 11:
-
-        if commit_id != '1f14723938e892801a9736481b588982d298ba48':
-           os.system('cd Repos/nilearn; git checkout ' +
-                     '1f14723938e892801a9736481b588982d298ba48')
-
-        os.system('./BugRuns/bug_id_11.sh')
-
-    if bug_id == 12:
-
-        if commit_id != '0c2c84aabb69ca73f2d78c3488b7afc2bc130ab3':
-           os.system('cd Repos/nilearn; git checkout ' +
-                     '0c2c84aabb69ca73f2d78c3488b7afc2bc130ab3')
-
-        os.system('./BugRuns/bug_id_12.sh')
-
-    if bug_id == 13:
-
-        if commit_id != '398ab45ba1a611ace5c603f04ee4fa8ef1ff115c':
-           os.system('cd Repos/nilearn; git checkout ' +
-                     '398ab45ba1a611ace5c603f04ee4fa8ef1ff115c')
-
-        os.system('./BugRuns/bug_id_13.sh')
-
-    if bug_id == 14:
-
-        if commit_id != '8e972c4d382a74fb126f768a0ae02e01e817515c':
-           os.system('cd Repos/nilearn; git checkout ' +
-                     '8e972c4d382a74fb126f768a0ae02e01e817515c')
-
-        os.system('./BugRuns/bug_id_14.sh')
-    
-    if bug_id == 15:
-
-        if commit_id != '99d878d6d78c3f5250bc0d627b651c66d9b9ec9f':
-           os.system('cd Repos/nilearn; git checkout ' +
-                     '99d878d6d78c3f5250bc0d627b651c66d9b9ec9f')
-
-        os.system('./BugRuns/bug_id_15.sh')
-
-def bug_test_lifelines(bug_id: int):
-
-    commit_id = get_commit_id('lifelines')
-
-    if bug_id == 16:
-
-        if commit_id != 'd2be007e7aea254670e244942d07df547beee603':
-           os.system('cd Repos/lifelines; git checkout ' +
-                     'd2be007e7aea254670e244942d07df547beee603')
-
-    if bug_id == 17:
-
-        if commit_id != 'cb22e7ffcfbcadf59c8bc3094206a1a2a46fd6cf':
-           os.system('cd Repos/lifelines; git checkout ' +
-                     'cb22e7ffcfbcadf59c8bc3094206a1a2a46fd6cf')
-    
-    if bug_id == 18:
-
-        if commit_id != '2a23205548e8c69115f145ca195899418dd380ee':
-           os.system('cd Repos/lifelines; git checkout ' +
-                     '2a23205548e8c69115f145ca195899418dd380ee')
-
-    if bug_id == 19:
-
-        if commit_id != '8f64de9d16425b232532977de43519bfbfdbb34a':
-           os.system('cd Repos/lifelines; git checkout ' +
-                     '8f64de9d16425b232532977de43519bfbfdbb34a')
-    
-    if bug_id == 20:
-
-        if commit_id != '3fe06bc23b53383721dd6efdd54b7ec9a454137d':
-           os.system('cd Repos/lifelines; git checkout ' +
-                     '3fe06bc23b53383721dd6efdd54b7ec9a454137d')
-
-    if bug_id == 21:
-
-        if commit_id != '37932adf0037a270d2bb8157c721bdf758a0c2c6':
-           os.system('cd Repos/lifelines; git checkout ' +
-                     '37932adf0037a270d2bb8157c721bdf758a0c2c6')
-
-    if bug_id == 22:
-
-        if commit_id != '519bd3abe6051bd9fb5da0dfffce24ab86171f3f':
-           os.system('cd Repos/lifelines; git checkout ' +
-                     '519bd3abe6051bd9fb5da0dfffce24ab86171f3f')
-
-def bug_test_nipype(bug_id: int):
-
-    commit_id = get_commit_id('nipype')
-
-    if bug_id == 23:
-
-        if commit_id != 'c06c03d4b65bcaa53674e671829743ffd7a2a615':
-           os.system('cd Repos/nipype; git checkout ' +
-                     'c06c03d4b65bcaa53674e671829743ffd7a2a615')
-
-    if bug_id == 24:
-
-        if commit_id != '29def823f78b880ef875681fd00aee54483746f6':
-           os.system('cd Repos/nipype; git checkout ' +
-                     '29def823f78b880ef875681fd00aee54483746f6')
-    
-    if bug_id == 25:
-
-        if commit_id != '6d087090ac556f7535452fa309f2373fb9c3db7a':
-           os.system('cd Repos/nipype; git checkout ' +
-                     '6d087090ac556f7535452fa309f2373fb9c3db7a')
-
-    if bug_id == 26:
-
-        if commit_id != 'e681974b00bf9d4aebef4b5dfd3f27cbbbf620f0':
-           os.system('cd Repos/nipype; git checkout ' +
-                     'e681974b00bf9d4aebef4b5dfd3f27cbbbf620f0')
-
-    if bug_id == 27:
-
-        if commit_id != 'dd2fb79fd9d75823b022907c987f3649e7360a56':
-           os.system('cd Repos/nipype; git checkout ' +
-                     'dd2fb79fd9d75823b022907c987f3649e7360a56')
-
-    if bug_id == 28:
-
-        if commit_id != '7295e6717c7ce2372792131dc0137f089da0f627':
-           os.system('cd Repos/nipype; git checkout ' +
-                     '7295e6717c7ce2372792131dc0137f089da0f627')
- 
-def bug_test_dipy(bug_id: int):
-
-    commit_id = get_commit_id('dipy')
-
-    if bug_id == 29:
-
-        if commit_id != 'd78e60b64a52cd140eb302d9a818d186cf560cf9':
-           os.system('cd Repos/dipy; git checkout ' +
-                     'd78e60b64a52cd140eb302d9a818d186cf560cf9')
-
-    if bug_id == 30:
-
-        if commit_id != 'f149c756e09f172c3b77a9e6c5b5390cc08af6ea':
-           os.system('cd Repos/dipy; git checkout ' +
-                     'f149c756e09f172c3b77a9e6c5b5390cc08af6ea')
-        
-
 def main():
 
     # Arguments vector (argv)
     parser = argparse.ArgumentParser(description='Test bugs based on ID')
 
-    parser.add_argument('bug_ids', metavar='B', type=int, nargs='+',
+    parser.add_argument('bug_ids', metavar='B', type=str, nargs='+',
                         help='Bugs to be tested.')
 
     args = parser.parse_args()
 
     bug_ids = args.bug_ids
 
-    # Load all repos in to map and associate the init function.
-    bug_dictionary = dict()
+    if bug_ids[0] == 'ALL':
+        bug_ids = [str(n+1) for n in range(30)]
 
-    bug_dictionary[1] = bug_test_breast_cancer_classifier
-    bug_dictionary[2] = bug_test_medical_zoo_pytorch
-    bug_dictionary[3] = bug_test_medical_zoo_pytorch
-    bug_dictionary[4] = bug_test_RGAN
-    bug_dictionary[5] = bug_test_nilearn
-    bug_dictionary[6] = bug_test_nilearn
-    bug_dictionary[7] = bug_test_nilearn
-    bug_dictionary[8] = bug_test_nilearn
-    bug_dictionary[9] = bug_test_nilearn
-    bug_dictionary[10] = bug_test_nilearn
-    bug_dictionary[11] = bug_test_nilearn
-    bug_dictionary[12] = bug_test_nilearn
-    bug_dictionary[13] = bug_test_nilearn
-    bug_dictionary[14] = bug_test_nilearn
-    bug_dictionary[15] = bug_test_nilearn
-    bug_dictionary[16] = bug_test_lifelines
-    bug_dictionary[17] = bug_test_lifelines
-    bug_dictionary[18] = bug_test_lifelines
-    bug_dictionary[19] = bug_test_lifelines
-    bug_dictionary[20] = bug_test_lifelines
-    bug_dictionary[21] = bug_test_lifelines
-    bug_dictionary[22] = bug_test_lifelines
-    bug_dictionary[23] = bug_test_nipype
-    bug_dictionary[24] = bug_test_nipype
-    bug_dictionary[25] = bug_test_nipype
-    bug_dictionary[26] = bug_test_nipype
-    bug_dictionary[27] = bug_test_nipype
-    bug_dictionary[28] = bug_test_nipype
-    bug_dictionary[29] = bug_test_dipy
-    bug_dictionary[30] = bug_test_dipy
+    # Load all repos in to map and associate the init function.
+    with open('commit_id.json') as json_file:
+        bug_dictionary = json.load(json_file)
 
     bugs = bug_dictionary.keys()
 
     # Process all args and call the function to init the desired repos.
     for bug_id in bug_ids:
-        bug_dictionary[bug_id](bug_id)
+        set_id_run_bug(bug_id, bug_dictionary[bug_id][0], bug_dictionary[bug_id][1])
     
 if __name__ == '__main__':
 

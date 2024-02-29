573bd2876a35f181681df3a4211f6a20f895d74c
GunaJaganathan <gunasjaganathan@gmail.com>
2024-02-29 13:27:17 -0500

    Updated scripts for bugs #1170, #418, #3200

--- test_scripts/lifelines/1170.py
+++ test_scripts/lifelines/1170.py
@@ -1,11 +1,23 @@
 from lifelines import WeibullFitter, KaplanMeierFitter
+from lifelines.datasets import load_waltons
+import numpy.testing as npt
+import operator
 
 def test_label_is_not_overwritten():
         fitter = WeibullFitter(label="Weibull")
         fitter.fit([1, 2, 3, 4], event_observed=[1, 1, 1, 1])
         assert fitter._label == "Weibull"
-
         fitter = KaplanMeierFitter(label="KM")
         fitter.fit([1, 2, 3, 4], event_observed=[1, 1, 1, 1])
         assert fitter._label == "KM"
 test_label_is_not_overwritten()
+
+def test_different_label(label1, label2):
+        waltons = load_waltons()
+        wbfL1 = WeibullFitter(label=label1)
+        wbfL1.fit(waltons['T'], waltons['E'])
+        wbfL2 = WeibullFitter(label=label2)
+        wbfL2.fit(waltons['T'], waltons['E'])
+        npt.assert_array_compare(operator.__ne__, wbfL1, wbfL2)
+
+test_different_label("Weibull", "KM")
--- test_scripts/nilearn/3200.py
+++ test_scripts/nilearn/3200.py
@@ -7,88 +7,160 @@ motor_images = datasets.fetch_neurovault_motor_task()
 stat_img = motor_images.images[0]
 
 threshold = 3.0
-table1 = get_clusters_table(stat_img, threshold, cluster_threshold=10)
-table2 = get_clusters_table(stat_img, threshold, cluster_threshold=0)
+print(stat_img)
+table1 = get_clusters_table(stat_img, threshold, cluster_threshold=4)
+table2 = get_clusters_table(stat_img, threshold, cluster_threshold=None)
 print(table1)
 print(table2)
-def test_threshold_img_with_cluster_threshold():
-    """Check that threshold_img behaves as expected with cluster_threshold
-    and/or two_sided.
-    """
-    # First we create a statistical image with specific characteristics
-    # shape = (20, 20, 30)
-    # affine = np.eye(4)
-    # data = np.zeros(shape, dtype=int)
-    # data[:2, :2, :2] = 4  # 8-voxel positive cluster
-    # data[4:6, :2, :2] = -4  # 8-voxel negative cluster
-    # data[8:11, 0, 0] = 5  # 3-voxel positive cluster
-    # data[13:16, 0, 0] = -5  # 3-voxel positive cluster
-    # data[:6, 4:10, :6] = 1  # 216-voxel positive cluster with low value
-    # data[13:19, 4:10, :6] = -1  # 216-voxel negative cluster with low value
-    # stat_img = Nifti1Image(data, affine, dtype=np.int16)
-    stat_img = motor_images.images[0]
-    # The standard approach should retain any clusters with values > 2
-    thr_img = threshold_img(stat_img, threshold=2, two_sided=False, copy=True)
-    print(np.unique(thr_img.get_fdata()))
-    #assert np.array_equal(np.unique(thr_img.get_fdata()), np.array([0, 4, 5]))
-    # With two-sided we get any clusters with |values| > 2
-    thr_img = threshold_img(stat_img, threshold=2, two_sided=True, copy=True)
-    print(np.unique(thr_img.get_fdata()))
-    # assert np.array_equal(
-    #     np.unique(thr_img.get_fdata()),
-    #     np.array([-5, -4, 0, 4, 5]),
-    # )
-    # With a cluster threshold of 5 we get clusters with |values| > 2 and
-    # cluster sizes > 5
-    thr_img = threshold_img(
-        stat_img,
-        threshold=2,
-        two_sided=True,
-        cluster_threshold=5,
-        copy=True,
-    )
-    print(np.unique(thr_img.get_fdata()))
-    #assert np.array_equal(np.unique(thr_img.get_fdata()), np.array([-4, 0, 4]))
+# def test_threshold_img_with_cluster_threshold():
+#     # stat_img = Nifti1Image(data, affine, dtype=np.int16)
+#     stat_img = motor_images.images[0]
+#     thr_img = threshold_img(stat_img, threshold=2, two_sided=False, copy=True)
+#     print(np.unique(thr_img.get_fdata()))
+#     thr_img = threshold_img(stat_img, threshold=2, two_sided=True, copy=True)
+#     print(np.unique(thr_img.get_fdata()))
+#     thr_img = threshold_img(
+#         stat_img,
+#         threshold=2,
+#         two_sided=True,
+#         cluster_threshold=5,
+#         copy=True,
+#     )
+#     print(np.unique(thr_img.get_fdata()))
+#     thr_img = threshold_img(
+#         stat_img,
+#         threshold=0.5,
+#         two_sided=True,
+#         cluster_threshold=5,
+#         copy=True,
+#     )
+#     print(np.unique(thr_img.get_fdata()))
+#     thr_img = threshold_img(
+#         stat_img,
+#         threshold=0.5,
+#         two_sided=False,
+#         cluster_threshold=5,
+#         copy=True,
+#     )
+#     print(np.unique(thr_img.get_fdata()))
+ 
+# test_threshold_img_with_cluster_threshold()
 
-    #assert np.sum(thr_img.get_fdata() == 4) == 8
-    # With a cluster threshold of 5 we get clusters with |values| > 0.5 and
-    # cluster sizes > 5
-    thr_img = threshold_img(
-        stat_img,
-        threshold=0.5,
-        two_sided=True,
-        cluster_threshold=5,
-        copy=True,
-    )
-    print(np.unique(thr_img.get_fdata()))
-    # assert np.array_equal(
-    #     np.unique(thr_img.get_fdata()),
-    #     np.array([-4, -1, 0, 1, 4]),
-    # )
-    # Now we disable two_sided again to get clusters with values > 0.5 and
-    # cluster sizes > 5
-    thr_img = threshold_img(
-        stat_img,
-        threshold=0.5,
-        two_sided=False,
-        cluster_threshold=5,
-        copy=True,
-    )
-    print(np.unique(thr_img.get_fdata()))
-    #assert np.array_equal(np.unique(thr_img.get_fdata()), np.array([0, 1, 4]))
+#   Cluster ID     X     Y     Z  Peak Stat Cluster Size (mm3)
+# 0          1  39.0 -22.0  58.0   7.941345              58347
+# 1         1a  42.0 -19.0  19.0   7.941345                   
+# 2         1b   6.0 -10.0  52.0   7.941345                   
+# 3         1c  33.0  -7.0  -2.0   7.905312                   
+# 4          2 -18.0 -52.0 -23.0   7.941345              10044
+# 5         2a  -6.0 -67.0 -38.0   3.443122                   
+# 6          3  -6.0 -70.0 -41.0   3.627778                 54
+#    Cluster ID     X     Y     Z  Peak Stat Cluster Size (mm3)
+# 0           1  39.0 -22.0  58.0   7.941345              60399
+# 1          1a  42.0 -19.0  19.0   7.941345                   
+# 2          1b   6.0 -10.0  52.0   7.941345                   
+# 3          1c  33.0  -7.0  -2.0   7.905312                   
+# 4           2 -18.0 -52.0 -23.0   7.941345              10260
+# 5          2a  -6.0 -70.0 -38.0   4.260736                   
+# 6           3  60.0   8.0  28.0   3.358555                108
+# 7           4 -66.0 -25.0  31.0   3.338923                351
+# 8           5  54.0  -1.0   7.0   3.287375                108
+# 9           6 -15.0 -94.0 -11.0   3.236299                 81
+# 10          7 -57.0  -1.0  40.0   3.020055                 27
+# 11          8 -60.0  -4.0  40.0   3.018578                 27
+# 12          9  45.0 -58.0  -2.0   3.007471                 27
 
-# if table1 == table2:
-#     print("pass")
-# else:
-#     print("fail")
+#   Cluster ID     X     Y     Z  Peak Stat Cluster Size (mm3)
+# 0          1  39.0 -22.0  58.0   7.941345              60399
+# 1         1a  42.0 -19.0  19.0   7.941345                   
+# 2         1b   6.0 -10.0  52.0   7.941345                   
+# 3         1c  33.0  -7.0  -2.0   7.905312                   
+# 4          2 -18.0 -52.0 -23.0   7.941345              10260
+# 5         2a  -6.0 -70.0 -38.0   4.260736                   
+# 6          3 -66.0 -25.0  31.0   3.338923                351
+#    Cluster ID     X     Y     Z  Peak Stat Cluster Size (mm3)
+# 0           1  39.0 -22.0  58.0   7.941345              60399
+# 1          1a  42.0 -19.0  19.0   7.941345                   
+# 2          1b   6.0 -10.0  52.0   7.941345                   
+# 3          1c  33.0  -7.0  -2.0   7.905312                   
+# 4           2 -18.0 -52.0 -23.0   7.941345              10260
+# 5          2a  -6.0 -70.0 -38.0   4.260736                   
+# 6           3  60.0   8.0  28.0   3.358555                108
+# 7           4 -66.0 -25.0  31.0   3.338923                351
+# 8           5  54.0  -1.0   7.0   3.287375                108
+# 9           6 -15.0 -94.0 -11.0   3.236299                 81
+# 10          7 -57.0  -1.0  40.0   3.020055                 27
+# 11          8 -60.0  -4.0  40.0   3.018578                 27
+# 12          9  45.0 -58.0  -2.0   3.007471                 27
 
-test_threshold_img_with_cluster_threshold()
+#  Cluster ID     X     Y     Z  Peak Stat Cluster Size (mm3)
+# 0          1  39.0 -22.0  58.0   7.941345              60399
+# 1         1a  42.0 -19.0  19.0   7.941345                   
+# 2         1b   6.0 -10.0  52.0   7.941345                   
+# 3         1c  33.0  -7.0  -2.0   7.905312                   
+# 4          2 -18.0 -52.0 -23.0   7.941345              10260
+# 5         2a  -6.0 -70.0 -38.0   4.260736                   
+# 6          3 -66.0 -25.0  31.0   3.338923                351
+#    Cluster ID     X     Y     Z  Peak Stat Cluster Size (mm3)
+# 0           1  39.0 -22.0  58.0   7.941345              60399
+# 1          1a  42.0 -19.0  19.0   7.941345                   
+# 2          1b   6.0 -10.0  52.0   7.941345                   
+# 3          1c  33.0  -7.0  -2.0   7.905312                   
+# 4           2 -18.0 -52.0 -23.0   7.941345              10260
+# 5          2a  -6.0 -70.0 -38.0   4.260736                   
+# 6           3  60.0   8.0  28.0   3.358555                108
+# 7           4 -66.0 -25.0  31.0   3.338923                351
+# 8           5  54.0  -1.0   7.0   3.287375                108
+# 9           6 -15.0 -94.0 -11.0   3.236299                 81
+# 10          7 -57.0  -1.0  40.0   3.020055                 27
+# 11          8 -60.0  -4.0  40.0   3.018578                 27
+# 12          9  45.0 -58.0  -2.0   3.007471                 27
 
-# [0.         2.00077105 2.00149131 ... 7.93853569 7.93925905 7.94134521]
-# [-7.9414444  -7.92836809 -7.90569019 ...  7.93853569  7.93925905
-#   7.94134521]
-# [-7.9414444  -7.92836809 -7.90569019 ...  7.93853569  7.93925905
-#   7.94134521]
-# [-7.9414444  -7.92836809 -7.90569019 ...  7.93853569  7.93925905
-#   7.94134521]
-# [0.         0.50000864 0.50004089 ... 7.93853569 7.93925905 7.94134521]
+#   Cluster ID     X     Y     Z  Peak Stat Cluster Size (mm3)
+# 0          1  39.0 -22.0  58.0   7.941345              60399
+# 1         1a  42.0 -19.0  19.0   7.941345                   
+# 2         1b   6.0 -10.0  52.0   7.941345                   
+# 3         1c  33.0  -7.0  -2.0   7.905312                   
+# 4          2 -18.0 -52.0 -23.0   7.941345              10260
+# 5         2a  -6.0 -70.0 -38.0   4.260736                   
+# 6          3  60.0   8.0  28.0   3.358555                108
+# 7          4 -66.0 -25.0  31.0   3.338923                351
+# 8          5  54.0  -1.0   7.0   3.287375                108
+# 9          6 -15.0 -94.0 -11.0   3.236299                 81
+#    Cluster ID     X     Y     Z  Peak Stat Cluster Size (mm3)
+# 0           1  39.0 -22.0  58.0   7.941345              60399
+# 1          1a  42.0 -19.0  19.0   7.941345                   
+# 2          1b   6.0 -10.0  52.0   7.941345                   
+# 3          1c  33.0  -7.0  -2.0   7.905312                   
+# 4           2 -18.0 -52.0 -23.0   7.941345              10260
+# 5          2a  -6.0 -70.0 -38.0   4.260736                   
+# 6           3  60.0   8.0  28.0   3.358555                108
+# 7           4 -66.0 -25.0  31.0   3.338923                351
+# 8           5  54.0  -1.0   7.0   3.287375                108
+# 9           6 -15.0 -94.0 -11.0   3.236299                 81
+# 10          7 -57.0  -1.0  40.0   3.020055                 27
+# 11          8 -60.0  -4.0  40.0   3.018578                 27
+# 12          9  45.0 -58.0  -2.0   3.007471                 27
+
+#   Cluster ID     X     Y     Z  Peak Stat Cluster Size (mm3)
+# 0          1  39.0 -22.0  58.0   7.941345              59832
+# 1         1a  42.0 -19.0  19.0   7.941345                   
+# 2         1b   6.0 -10.0  52.0   7.941345                   
+# 3         1c  33.0  -7.0  -2.0   7.905312                   
+# 4          2 -18.0 -52.0 -23.0   7.941345              10206
+# 5         2a  -6.0 -70.0 -38.0   4.260736                   
+# 6          3  60.0   8.0  28.0   3.358555                108
+# 7          4 -66.0 -25.0  31.0   3.338923                189
+#    Cluster ID     X     Y     Z  Peak Stat Cluster Size (mm3)
+# 0           1  39.0 -22.0  58.0   7.941345              60399
+# 1          1a  42.0 -19.0  19.0   7.941345                   
+# 2          1b   6.0 -10.0  52.0   7.941345                   
+# 3          1c  33.0  -7.0  -2.0   7.905312                   
+# 4           2 -18.0 -52.0 -23.0   7.941345              10260
+# 5          2a  -6.0 -70.0 -38.0   4.260736                   
+# 6           3  60.0   8.0  28.0   3.358555                108
+# 7           4 -66.0 -25.0  31.0   3.338923                351
+# 8           5  54.0  -1.0   7.0   3.287375                108
+# 9           6 -15.0 -94.0 -11.0   3.236299                 81
+# 10          7 -57.0  -1.0  40.0   3.020055                 27
+# 11          8 -60.0  -4.0  40.0   3.018578                 27
+# 12          9  45.0 -58.0  -2.0   3.007471                 27
